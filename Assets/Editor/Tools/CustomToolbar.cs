a20174e36be8fd279786e6a3c4e94722112bfa9c
dinghuilin <632636416@qq.com>
2020-04-19 17:28:30 +0800

    提交启动游戏工具

---
+++ Assets/Editor/Tools/CustomToolbar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace EditorExtend
+{
+    [InitializeOnLoad]
+    public static class CustomToolbar
+    {
+        static CustomToolbar()
+        {
+            ToolbarExtend.OnLeftToolbarGUI += OnLeftGUI;
+        }
+
+        private static void OnLeftGUI()
+        {
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button(new GUIContent("G", "Start Game"), ToolbarExtend.GetCommandButtonStyle()))
+            {
+                EditorPrefs.SetBool(URLSetting.START_IS_GAME, true);
+                EditorApplication.ExecuteMenuItem("Edit/Play");
+            }
+        }
+    }
+}
