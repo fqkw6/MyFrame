5b46cdf2f458251f41a354c81ddcf4faef1031da
dinghuilin <632636416@qq.com>
2020-04-25 13:06:46 +0800

    支持python3.7

--- ConfigData/Excel2Lua/excel2lua.py
+++ ConfigData/Excel2Lua/excel2lua.py
@@ -1,4 +1,4 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 
 import sys
 import os
@@ -6,8 +6,7 @@ import xlrd
 import re
 
 # 使可以解析中文字符
-reload(sys)
-sys.setdefaultencoding('utf8')
+
 
 # 当前脚本路径
 curpath = os.path.dirname(os.path.abspath(sys.argv[0]))
@@ -23,21 +22,26 @@ lua_file_head_format_desc = '''--[[
 --]]\n\n'''
 
 # 将数据导出到tgt_lua_path
+
+
 def toInt(val):
-    if isinstance(val, unicode):
+    # if isinstance(val, str):
+    #     v = int(filter(str.isdigit, val.encode("utf-8")))
+    if isinstance(val, str):
         v = int(filter(str.isdigit, val.encode("utf-8")))
-    elif isinstance(val, str):
-        v = int(val)
     else:
         v = int(val)
-    return v;
+    return v
 
 # 将数据导出到tgt_lua_path
+
+
 def excel2lua(src_excel_path, tgt_lua_path):
     # print('[file] %s -> %s' % (src_excel_path, tgt_lua_path))
     # print('[file] %s -> %s' % (os.path.basename(src_excel_path), os.path.basename(tgt_lua_path)))
     # load excel data
-    excel_data_src = xlrd.open_workbook(src_excel_path, encoding_override = 'utf-8')
+    excel_data_src = xlrd.open_workbook(
+        src_excel_path, encoding_override='utf-8')
     # print('[excel] Worksheet names : %s'%excel_data_src.sheet_names())
 
     # get first Sheet
@@ -50,7 +54,7 @@ def excel2lua(src_excel_path, tgt_lua_path):
     # col name list
     col_name_list = []
 
-    #col val type list
+    # col val type list
     col_val_type_list = []
 
     # ctype: 0 empty, 1 string, 2 number, 3 date, 4 boolean, 5 error
@@ -82,7 +86,8 @@ def excel2lua(src_excel_path, tgt_lua_path):
 
         # 检查id的唯一性
         if cell_id.value in excel_data_dict:
-            print('[warning] duplicated data id: "%d", all previous value will be ignored!~' % (cell_id.value))
+            print('[warning] duplicated data id: "%d", all previous value will be ignored!~' % (
+                cell_id.value))
 
         # row data list
         row_data_list = []
@@ -133,7 +138,8 @@ def excel2lua(src_excel_path, tgt_lua_path):
         excel_data_dict[cell_id.value] = row_data_list
 
     # 正则搜索lua文件名 不带后缀 用作table的名称 练习正则的使用
-    searchObj = re.search(r'([^\\/:*?"<>|\r\n]+)\.\w+$', tgt_lua_path, re.M|re.I)
+    searchObj = re.search(r'([^\\/:*?"<>|\r\n]+)\.\w+$',
+                          tgt_lua_path, re.M | re.I)
     lua_table_name = searchObj.group(1)
     # print('正则匹配:', lua_table_name, searchObj.group(), searchObj.groups())
 
@@ -142,10 +148,12 @@ def excel2lua(src_excel_path, tgt_lua_path):
     tgt_lua_file_name = os.path.basename(tgt_lua_path)
 
     # file head desc
-    lua_file_head_desc = lua_file_head_format_desc % (tgt_lua_file_name, src_excel_file_name)
+    lua_file_head_desc = lua_file_head_format_desc % (
+        tgt_lua_file_name, src_excel_file_name)
 
     # export to lua file
-    lua_export_file = open(tgt_lua_path, 'w')
+    # encoding='utf-8'储存为utf-8格式
+    lua_export_file = open(tgt_lua_path, 'w', encoding='utf-8')
     lua_export_file.write(lua_file_head_desc)
     lua_export_file.write('local %s = {\n' % lua_table_name)
 
@@ -153,15 +161,18 @@ def excel2lua(src_excel_path, tgt_lua_path):
     for k, v in excel_data_dict.items():
         lua_export_file.write('    [%d] = {\n' % toInt(k))
         for row_data in v:
-            lua_export_file.write('        {0} = {1},\n'.format(row_data[0], row_data[1]))
+            lua_export_file.write(
+                '        {0} = {1},\n'.format(row_data[0], row_data[1]))
         lua_export_file.write('    },\n')
 
     lua_export_file.write('}\n')
-    lua_export_file.write('return %s'%lua_table_name)
+    lua_export_file.write('return %s' % lua_table_name)
 
     lua_export_file.close()
 
-    print('[excel] %d row data exported !~ %s' % (excel_sheet.nrows,os.path.basename(tgt_lua_path)))
+    print('[excel] %d row data exported !~ %s' %
+          (excel_sheet.nrows, os.path.basename(tgt_lua_path)))
+
 
 # Make a script both importable and executable (∩_∩)
 if __name__ == '__main__':
@@ -169,8 +180,9 @@ if __name__ == '__main__':
         print('python excel2lua.py <excel_input_path> <lua_output_path>')
         exit(1)
 
-    excel2lua(os.path.join(curpath, sys.argv[1]), os.path.join(curpath, sys.argv[2]))
-    
+    excel2lua(os.path.join(curpath, sys.argv[1]), os.path.join(
+        curpath, sys.argv[2]))
+
     # excelPath = curpath+"/Hero.xlsx";
     # luaPath = curpath+"/Hero.lua";
     # excel2lua(excelPath, luaPath)
