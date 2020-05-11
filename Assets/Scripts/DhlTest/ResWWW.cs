5a1a63737ae58587cbf7f44117895bdc8e764c8b
dinghuilin <632636416@qq.com>
2020-05-11 11:46:04 +0800

    提交资源

---
+++ Assets/Scripts/DhlTest/ResWWW.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResWWW : MonoBehaviour
+{
+    public string url = "http://www.manew.com/template/manew2016/images/logo.png";
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
