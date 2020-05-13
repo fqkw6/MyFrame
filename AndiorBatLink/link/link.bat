6d0d7a80b7971637a5a8f620f92afd7ab4cd4296
dinghuilin <632636416@qq.com>
2020-05-13 10:02:17 +0800

    link

---
+++ AndiorBatLink/link/link.bat
@@ -0,0 +1,3 @@
+rem C 盘的东西映射到d盘
+junction "F:\ABServer\ABServer" "F:\work\MyFrame\AssetBundles" 
+pause
---
+++ AndiorBatLink/test1.bat
@@ -0,0 +1,2 @@
+D:\android\platform-tools\adb.exe logcat  -s time Unity:e -d > D:\link\1.txt
+pause
