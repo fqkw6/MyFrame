f3237075b81a5efa771af27c7b70038bb8509a8a
dinghuilin <632636416@qq.com>
2020-05-13 23:07:22 +0800

    打印面板预设路径

---
+++ Assets/Editor/Tools/GameObjectCopyPath.cs
@@ -0,0 +1,32 @@
+using UnityEditor;
+using UnityEngine;
+
+public class GameObjectCopyPath : Editor
+{
+    private static TextEditor textEditor = new TextEditor();
+
+    [MenuItem("GameObject/CopyPath _F12", priority = 12)]
+    private static void NewMenuOption()
+    {
+        if (Selection.activeGameObject)
+        {
+            string s = GetTransPath(Selection.activeGameObject.transform);
+            textEditor.text = s;
+            textEditor.OnFocus();
+            textEditor.Copy();
+            Debug.Log("[CopyPath]:" + s);
+        }
+    }
+
+    /// <summary>
+    /// 获得GameObject在Hierarchy中的完整路径
+    /// </summary>
+    public static string GetTransPath(Transform trans)
+    {
+        if (!trans.parent)
+        {
+            return trans.name;
+        }
+        return GetTransPath(trans.parent) + "/" + trans.name;
+    }
+}
