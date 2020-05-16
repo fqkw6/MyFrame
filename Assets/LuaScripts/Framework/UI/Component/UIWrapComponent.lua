ffd686a6b7e896e338747f56a491af0e956ccc56
dinghuilin <632636416@qq.com>
2020-05-17 00:28:59 +0800

    添加lua 注释

--- Assets/LuaScripts/Framework/UI/Component/UIWrapComponent.lua
+++ Assets/LuaScripts/Framework/UI/Component/UIWrapComponent.lua
@@ -4,19 +4,17 @@
 -- 注意：
 -- 1、这里只是作为一个接口文件来使用，具体功能需要自行继承并实现
 --]]
-
+---@class UIWrapComponent :UIBaseContainer
 local UIWrapComponent = BaseClass("UIWrapComponent", UIBaseContainer)
+---@return UIBaseContainer
 local base = UIBaseContainer
 
 -- 组件被复用时回调该函数，执行组件的刷新
-local function OnRefresh(self, real_index, check)
+function UIWrapComponent:OnRefresh(real_index, check)
 end
 
 -- 组件添加了按钮组，则按钮被点击时回调该函数
-local function OnClick(self, toggle_btn, real_index, check)
+function UIWrapComponent:OnClick(toggle_btn, real_index, check)
 end
-
-UIWrapComponent.OnRefresh = OnRefresh
-UIWrapComponent.OnClick = OnClick
 
 return UIWrapComponent
