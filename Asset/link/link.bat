46d0bf98f2fc69fa4aa7ade7af592bba20d9ff19
dinghuilin <632636416@qq.com>
2020-05-22 13:20:53 +0800

    删除

--- Asset/link/link.bat
+++
@@ -1,3 +0,0 @@
-rem C 盘的东西映射到d盘
-junction "F:\ABServer\ABServer" "F:\work\MyFrame\AssetBundles" 
-pause
--- Asset/link/test1.bat
+++
@@ -1,2 +0,0 @@
-D:\android\platform-tools\adb.exe logcat  -s time Unity:e -d > D:\link\1.txt
-pause
