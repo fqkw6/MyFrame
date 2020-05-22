3cba594bbf2dfdf5bbf3c2d4ee5632037fa2987d
dinghuilin <632636416@qq.com>
2020-05-22 12:12:46 +0800

    提交

--- Assets/LuaScripts/UnitTest/ProtobufTest.lua
+++ Assets/LuaScripts/UnitTest/ProtobufTest.lua
@@ -1,30 +1,31 @@
 local pb = require "pb"
-
+local bytes
 function InitPB()
     --初始化pb文件，路径采用绝对路径
     assert(pb.loadfile "Assets/LuaScripts/Net/Proto/login.pb")
 end
-
-function Encoder()
+local function Encoder()
     local data = {
-        username = "666",
-        password = "777"
+        flag = "666"
     }
 
     -- encode lua table data into binary format in lua string and return
-    local bytes = assert(pb.encode("login.req_login", data))
-    print(pb.tohex(bytes))
+    bytes = assert(pb.encode("login.req_login", data))
+    ---压缩
+    Logger.LogError(pb.tohex(bytes))
     return bytes
 end
 
 function Decoder(pb_data)
     -- and decode the binary data back into lua table
     local data2 = assert(pb.decode("login.req_login", bytes))
-    print(data2.username)
+    ---解压
+    -- Logger.LogError("cesss")
+    -- Logger.LogError(data2.flag)
 end
 
 local function Run()
-    print("------------Test lua_PB------------")
+    -- Logger.LogError("------------Test lua_PB------------")
 
     InitPB()
 
